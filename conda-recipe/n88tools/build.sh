00ddf86a034cf316ff51af4822ee71b6fabef59f
friedchickean <kdarg29@gmail.com>
2025-05-28 15:44:26 -0600

    Set XCode 14.1 as sysroot for macos-13

--- conda-recipe/n88tools/build.sh
+++ conda-recipe/n88tools/build.sh
@@ -1,3 +1,14 @@
 set -x
 
+case $(uname | tr '[:upper:]' '[:lower:]') in
+  linux*)
+		# Nothing to do
+    ;;
+  darwin*)
+		export CFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=14.1 ${CFLAGS}"
+		export CXXFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=14.1 ${CXXFLAGS}"
+    ;;
+  *)
+esac
+
 ${PYTHON} -m pip install . --no-deps --ignore-installed -vv
