5f218562ded8859d3db98ad951399a0f5724e462
friedchickean <kdarg29@gmail.com>
2025-05-29 09:31:41 -0600

    Hardcode sysroot to override conda build's default value

--- conda-recipe/n88tools/build.sh
+++ conda-recipe/n88tools/build.sh
@@ -5,8 +5,8 @@ case $(uname | tr '[:upper:]' '[:lower:]') in
 		# Nothing to do
     ;;
   darwin*)
-		export CFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=14.1 ${CFLAGS}"
-		export CXXFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=14.1 ${CXXFLAGS}"
+		export CFLAGS="-isysroot /Applications/Xcode_14.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -mmacosx-version-min=14.1 ${CFLAGS}"
+		export CXXFLAGS="-isysroot /Applications/Xcode_14.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -mmacosx-version-min=14.1 ${CXXFLAGS}"
     ;;
   *)
 esac
