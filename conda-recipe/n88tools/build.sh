01101035668787e01bd9c15b015962bf5c90f5b5
friedchickean <kdarg29@gmail.com>
2025-05-29 10:56:58 -0600

    Build without setting flags and pinning tapi

--- conda-recipe/n88tools/build.sh
+++ conda-recipe/n88tools/build.sh
@@ -1,14 +1,3 @@
 set -x
 
-case $(uname | tr '[:upper:]' '[:lower:]') in
-  linux*)
-		# Nothing to do
-    ;;
-  darwin*)
-		export CFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} ${CFLAGS}"
-		export CXXFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} ${CXXFLAGS}"
-    ;;
-  *)
-esac
-
 ${PYTHON} -m pip install . --no-deps --ignore-installed -vv
